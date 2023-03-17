a226f9bbda3871b1e5188b44f61178737a30b3d5
Ivan Pozdin <ivanllpozdin@gmail.com>
2023-03-17 16:15:25 +0300

    fix: function's file is not in git

---
+++ HW2_Task4/Program.fs
@@ -0,0 +1,19 @@
+﻿let isPrime (n: bigint) =
+    let rec loop i =
+        if i * i > n then true
+        elif n % i = 0I then false
+        else loop (i + 1I)
+
+    if n < 2I then false else loop 2I
+
+let primes =
+    let findNextPrime n =
+        let rec findNextPrimeRec m =
+            if isPrime m then m else findNextPrimeRec (m + 1I)
+
+        let nextPrime = findNextPrimeRec (n + 2I)
+        Some(nextPrime, nextPrime)
+
+    Seq.unfold findNextPrime 2I |> Seq.append { 2I .. 3I }
+
+primes |> Seq.take 150 |> Seq.iter (printfn "%A")
